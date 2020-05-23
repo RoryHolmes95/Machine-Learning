d2fc5ee2303094015ff8d55af543970dc8034199
Rory Holmes <roryholmes@kubrickgroup.com>
2020-05-23 11:52:19 +0100

    Added for-loop/if-function to remove potentially dependent variables

--- Mult_Log_Func.py
+++ Mult_Log_Func.py
@@ -32,12 +32,12 @@ def multLogReg():
     columns = []
     for i in (cols):
         columns.append(i)
-    num = int(input(f"how many of the following fields have binary answers? {columns}"))
+    num = int(input(f"How many of the following fields have binary answers? {columns}"))
     lst = []
     for i in range(num):
         lst.append(0)
     for i in range(num):
-        lst[i] = input(f"one at a time, select those columns from the following: {columns} ")
+        lst[i] = input(f"One at a time, select those columns from the following: {columns} ")
     for i in lst:
         if len(data[i].unique()) > 2:
             return "One of those fields has more than 2 possible values, please rectify and then retry"
@@ -60,10 +60,10 @@ def multLogReg():
             reduced.dropna(subset = [i], inplace=True)
             print (f"The few rows with null values for the field '{i}' have been removed")
         elif (nulls[i] > 30) & (nulls[i] <= ((np.mean(reduced.count()))/3)):
-            print (f"The field '{i}', has too many null values, these need to first be filled in using averages")
+            print (f"The field '{i}', has too many null values, these will be filled in using user-defined averages")
             large_null += [i]
             if len(large_null) > 1:
-                return "Sorry, too many fields have too many null fields, please rectify  and try again"
+                return "Sorry, too many fields have too many null values, please rectify and try again"
             else:
                 continue
         elif nulls[i] > (np.mean(reduced.count())/3):
@@ -76,6 +76,11 @@ def multLogReg():
     grouping = reduced.groupby(reduced[(grouping_field)])[large_null[0]]
     means = (grouping.mean())
     reduced[large_null] = reduced[large_null].apply(avg_approx, axis = 1, args = (reduced, grouping_field, large_null[0], means))
+    for name in reduced.corr():
+        for each in reduced.corr()[name]:
+            if ((each < -0.5) & (each != -1.0)) | ((each > 0.5) & (each != 1.0)):
+                print (f"Unable to verify if two variables are independent, '{name}' has been dropped.")
+                reduced = reduced.drop([name], axis=1)
     binaries = []
     num_of_binaries = int(input("How many of the binary fields are remaining? Please do not include the predictor"))
     label_encoder = preprocessing.LabelEncoder()
@@ -91,7 +96,7 @@ def multLogReg():
             reduced = reduced.drop([i], axis = 1)
             reduced = pd.concat([reduced, DF], axis = 1)
     categoricals = []
-    cat_data = int(input(f"How many of the remaining feilds contain categorical answers with more than 2 possible answers? {reduced_columns}"))
+    cat_data = int(input(f"How many of the remaining fields contain categorical answers with more than 2 possible answers? {reduced_columns}"))
     reduced.dropna(inplace=True)
     if cat_data > 0:
         for i in range(cat_data):
@@ -115,7 +120,7 @@ def multLogReg():
             reduced = pd.concat([reduced, cat_DF], axis = 1)
     reduced.dropna(inplace=True)
     print (reduced.info())
-    print (reduced.isnull().sum())
+
 
 
 

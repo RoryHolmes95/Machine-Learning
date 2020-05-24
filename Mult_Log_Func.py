bb53e3962987530be7652291684810b7d5b26f24
Rory Holmes <roryholmes@kubrickgroup.com>
2020-05-24 18:39:29 +0100

    Rectified mistake confusing predictants with predictors

--- Mult_Log_Func.py
+++ Mult_Log_Func.py
@@ -28,13 +28,13 @@ def multLogReg():
        users choosing'''
     address = input("Copy and paste your csv file here")
     data = pd.read_csv(address)
-    predictor = input("What is your chosen predictor?")
+    predictant = input("What is your chosen predictant?")
     cols = data.columns
     columns = []
     for i in (cols):
         columns.append(i)
-    columns.remove(predictor)
-    num1 = int(input(f"how many of the following fields are neither the predictor or predictants? {columns}"))
+    columns.remove(predictant)
+    num1 = int(input(f"how many of the following fields are neither the predictant or predictors? {columns}"))
     to_drop = []
     for i in range(num1):
         to_drop.append(0)
@@ -44,7 +44,7 @@ def multLogReg():
     reduced_columns = []
     for i in (reduced):
         reduced_columns.append(i)
-    reduced_columns.remove(predictor)
+    reduced_columns.remove(predictant)
     nulls = reduced.isnull().sum()
     large_null = []
     for i in reduced:
@@ -64,7 +64,7 @@ def multLogReg():
         else:
             continue
     if len(large_null) > 0:
-        grouping_field = input(f"'{large_null[0]}' has too many null values, choose a predictant to use in order to fill in the nulls with weighted averages: {reduced_columns}")
+        grouping_field = input(f"'{large_null[0]}' has too many null values, choose a predictor to use in order to fill in the nulls with weighted averages: {reduced_columns}")
         grouping = reduced.groupby(reduced[(grouping_field)])[large_null[0]]
         means = (grouping.mean())
         reduced[large_null] = reduced[large_null].apply(avg_approx, axis = 1, args = (reduced, grouping_field, large_null[0], means))
@@ -93,14 +93,14 @@ def multLogReg():
             reduced = reduced.drop([i], axis = 1)
             reduced = pd.concat([reduced, DF], axis = 1)
     if (np.mean(reduced.count())/len(reduced.columns)-1) > 50:
-        print ("Enough predictants to proceed...")
+        print ("Enough predictors to proceed...")
     else:
-        return "There are not enough fields for the number of predictants you have selected, please try again"
+        return "There are not enough fields for the number of predictors you have selected, please try again"
     categoricals = []
     red_cols = []
     for redcols in (reduced.columns):
         red_cols.append(redcols)
-    red_cols.remove(predictor)
+    red_cols.remove(predictant)
     cat_data = int(input(f"How many of the remaining fields contain categorical answers with more than 2 possible answers? {red_cols}"))
     reduced.dropna(inplace=True)
     if cat_data > 0:
@@ -124,22 +124,22 @@ def multLogReg():
             reduced = reduced.drop([i], axis = 1)
             reduced = pd.concat([reduced, cat_DF], axis = 1)
             reduced.dropna(inplace=True)
-    predictant_train, predictant_test, predictor_train, predictor_test = model_selection.train_test_split(reduced.drop([predictor], axis = 1), reduced[predictor], test_size = 0.2, random_state = 200)
+    predictor_train, predictor_test, predictant_train, predictant_test = model_selection.train_test_split(reduced.drop([predictant], axis = 1), reduced[predictant], test_size = 0.2, random_state = 200)
     LogReg = linear_model.LogisticRegression(solver = 'liblinear')
-    LogReg.fit(predictant_train, predictor_train)
-    predicting = LogReg.predict(predictant_test)
-    class_report = metrics.classification_report(predictor_test, predicting)
-    cross = model_selection.cross_val_predict(LogReg, predictant_train, predictor_train, cv = 5)
-    conf = metrics.confusion_matrix(predictor_train, cross)
+    LogReg.fit(predictor_train, predictant_train)
+    predicting = LogReg.predict(predictor_test)
+    class_report = metrics.classification_report(predictant_test, predicting)
+    cross = model_selection.cross_val_predict(LogReg, predictor_train, predictant_train, cv = 5)
+    conf = metrics.confusion_matrix(predictant_train, cross)
     print (f"Out of {sum(sum(conf))} results, there were {conf[0][1]} false positives, and {conf[1][0]} false negatives")
-    precision = metrics.precision_score(predictor_train, cross)
+    precision = metrics.precision_score(predictant_train, cross)
     test_passenger = []
-    num_of_columns = len(reduced.drop([predictor], axis = 1).columns)
+    num_of_columns = len(reduced.drop([predictant], axis = 1).columns)
     for col in range(num_of_columns):
         test_passenger += [0]
     for num in range(len(test_passenger)):
-        test_passenger[num] = int(input(f"One by one, fill in your predictions into the following predictants: {reduced.drop([predictor], axis = 1).columns}"))
-        print (f"{reduced.drop([predictor],axis=1).columns[num]} : {test_passenger[num]}")
+        test_passenger[num] = int(input(f"One by one, fill in your predictions into the following predictors: {reduced.drop([predictant], axis = 1).columns}"))
+        print (f"{reduced.drop([predictant],axis=1).columns[num]} : {test_passenger[num]}")
     test_passenger = np.array(test_passenger).reshape(1,-1)
     survived = (LogReg.predict(test_passenger))
     if survived[0] == 0:
